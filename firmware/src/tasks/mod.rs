b3ac1d23a8b1eccccb19d1a5f43af9830efa9e2a
Pablo Labarta <pablitolabarta@gmail.com>
2026-08-21 17:01:54 -0300

    chore: change name deloj debug

--- firmware/src/tasks/mod.rs
+++ firmware/src/tasks/mod.rs
@@ -3,7 +3,4 @@ pub mod control_spi_encoders;
 #[cfg(feature = "logging-encoders-raw")]
 pub use control_spi_encoders::control_spi_encoders;
 
-pub mod task1;
-pub use task1::task1;
-pub mod task2;
-pub mod task3;
+pub mod reloj_debug;
